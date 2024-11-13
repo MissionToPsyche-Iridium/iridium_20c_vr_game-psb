4e5734898f656bb5041c0e33aa23732b38ae5642
JohnPham <89952683+jmp7651@users.noreply.github.com>
2024-11-12 20:11:40 -0500

    importing popups and scripts from old game

---
+++ Assets/Scripts/Popup/Popup_object.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using popup_controller;
+
+public class Popup_object : MonoBehaviour
+{
+    private popup_controller popupC = popup_controller.getInstance();
+    private Interact_Distance ID = Interact_Distance.GetInsance();
+    private GameObject childwithImage;
+    [SerializeField] private AudioSource myAudioSource;
+
+    private void Awake()
+    {
+        GameObject currentObj = gameObject;
+        GameObject childObj;
+
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            childObj= transform.GetChild(i).gameObject;
+            if(childObj.GetComponent<Image>() != null)
+            {
+                childwithImage = childObj;
+                break;
+            }
+        }
+    }
+    void Update()
+    {
+        //if space is pressed, then clear popup
+        if(Input.GetKey(KeyCode.Space) & popupC.isPopupVisible())
+        {
+            popupC.clear_popup();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if(ID.checkDistance(5, gameObject) && !popupC.isPopupVisible())
+        {
+            myAudioSource.Play();
+            popupC.display_popup(childwithImage.GetComponent<Image>());
+        }
+    }
+}
---
+++ Assets/Scripts/Popup/camera.cs
@@ -0,0 +1,67 @@
+using PlasticPipe.PlasticProtocol.Messages;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.Oculus;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class camera
+{
+    private List<boundary> camBoundaries = new List<boundary>();
+    private Vector3 position;
+    private Vector3 angle;
+    private bool locked = false;
+
+    // TODO: Make camera settings changable
+    private float camSpeed = 0.055f;
+    private float camSensitivity = 0.45f;
+
+    public camera(Vector3 pos, Vector3 ang)
+    {
+        this.position = pos;
+        this.angle = ang;
+    }
+    public Vector3 getPos()
+    {
+        return position;
+    }
+    public void setPos(Vector3 pos)
+    {
+        this.position = pos;
+    }
+    public Vector3 getAng()
+    {
+        return angle;
+    }
+    public void setAng(Vector3 ang)
+    {
+        this.angle = ang;
+    }
+
+    public void addBoundaries(boundary boundary)
+    {
+        camBoundaries.Add(boundary);
+    }
+
+    public List<boundary> getBoundaries()
+    {
+        return camBoundaries;
+    }
+
+    public float getSpeed()
+    {
+        return camSpeed;
+    }
+    public float getSensitivity()
+    {
+        return camSensitivity;
+    }
+    public bool getLock()
+    {
+        return locked;
+    }
+    public void changeLock()
+    {
+        locked = !locked;
+    }
+}
