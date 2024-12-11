e94aaf71a98f7545323d737f5809e539efed0d3f
JohnNoodle <jmp7651@psu.edu>
2024-12-11 12:31:57 -0500

    update to main menu

--- Assets/Scripts/EventModeButtonManager.cs
+++ Assets/Scripts/EventModeButtonManager.cs
@@ -3,23 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-public class OfficeSceneTransition : MonoBehaviour
+public class EventModeButtonManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    //Load scene
     public void NextScene()
     {
         SceneManager.LoadScene("Office");
     }
+
 }
