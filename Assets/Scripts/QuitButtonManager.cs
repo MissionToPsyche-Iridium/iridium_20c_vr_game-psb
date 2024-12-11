b6630a68a2eacf8565d4fa9a3b13b1347cb77270
JohnNoodle <jmp7651@psu.edu>
2024-12-11 14:48:45 -0500

    fixing ui

--- Assets/Scripts/QuitButtonManager.cs
+++ Assets/Scripts/QuitButtonManager.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
 
 public class QuitButtonManager : MonoBehaviour
 {
     public GameObject pauseMenu;
     public GameObject settingsMenu;
+    public InputActionProperty showButton;
+
 
     public void NextScene()
     {
         SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1.0f;
     }
 
     public void closingSettingMenu()
@@ -18,6 +23,7 @@ public class QuitButtonManager : MonoBehaviour
         pauseMenu.transform.position = settingsMenu.transform.position;
         settingsMenu.SetActive(false);
         pauseMenu.SetActive(true);
+        showButton.action.Enable();
     }
 
 }
--- Assets/Scripts/SettingsButtonManager.cs
+++ Assets/Scripts/SettingsButtonManager.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SettingsButtonManager : MonoBehaviour
 {
     public GameObject pauseMenu;
     public GameObject settingsMenu;
+    public InputActionProperty showButton;
+
     public void setUpSettingMenu()
     {
         settingsMenu.transform.position = pauseMenu.transform.position;
+        settingsMenu.transform.rotation = pauseMenu.transform.rotation;
+        showButton.action.Disable();
         settingsMenu.SetActive(true);
         pauseMenu.SetActive(false);
     }
