470c8de34d41eff4d72e38dd9eaf16a63042396a
akv0812 <angelinavalentin0812@gmail.com>
2025-01-30 15:03:28 -0500

    game over scene color update and controller interaction

--- Assets/Scenes/GameOver/GameOverUI.cs
+++ Assets/Scenes/GameOver/GameOverUI.cs
@@ -6,8 +6,6 @@ using UnityEngine.EventSystems; // For handling UI events
 
 public class GameOverUI : MonoBehaviour
 {
-    public XRRayInteractor leftInteractor;  // XR Ray Interactor for the left controller
-    public XRRayInteractor rightInteractor; // XR Ray Interactor for the right controller
 
     public EventSystem eventSystem; // Reference to the event system
 
@@ -24,38 +22,4 @@ public class GameOverUI : MonoBehaviour
         Application.Quit();
     }
 
-    private void Update()
-    {
-        // Check if the left or right controller is interacting with an object
-        if (leftInteractor && leftInteractor.hasSelection)
-        {
-            HandleButtonPress(leftInteractor);
-        }
-        else if (rightInteractor && rightInteractor.hasSelection)
-        {
-            HandleButtonPress(rightInteractor);
-        }
-    }
-
-    // Handles button press based on the selected UI button
-    private void HandleButtonPress(XRRayInteractor interactor)
-    {
-        // Get the raycast hit information from the interactor
-        RaycastHit hit;
-        Ray ray = new Ray(interactor.transform.position, interactor.transform.forward);
-
-        if (Physics.Raycast(ray, out hit, 10f)) // Adjust the raycast distance as needed
-        {
-            // Check if the raycast hits a UI button
-            if (hit.collider.CompareTag("UIButton"))
-            {
-                Button button = hit.collider.GetComponent<Button>();  // Now it will recognize the Button class
-                if (button != null)
-                {
-                    // Trigger the button click via the EventSystem
-                    ExecuteEvents.Execute(button.gameObject, new PointerEventData(eventSystem), ExecuteEvents.pointerClickHandler);
-                }
-            }
-        }
-    }
 }
