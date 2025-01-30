60f309accd0aed8265ac0873d3ff7ed867c0cab7
jle5540 <166565405+jle5540@users.noreply.github.com>
2025-01-30 14:44:14 -0500

    updated OfficeManager to be able to change turning

--- Assets/Scripts/OfficeManager.cs
+++ Assets/Scripts/OfficeManager.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class OfficeManager : MonoBehaviour
 {
     public GameTimer timer;
     public GameObject timerCanvas;
     public GameObject textTimer;
+    public ActionBasedSnapTurnProvider snapTurn;
+    public ActionBasedContinuousTurnProvider continuousTurn;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         if(SettingManager.Instance.EventMode == false) {
             timer.enabled = false;
             timerCanvas.SetActive(false);
             textTimer.SetActive(false);
+        }
+
+        if(SettingManager.Instance.ContinuousTurn == false) {
+            snapTurn.enabled = true;
+            continuousTurn.enabled = false;
         }
     }
 
