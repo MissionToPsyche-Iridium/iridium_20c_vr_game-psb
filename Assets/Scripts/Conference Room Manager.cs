7bfe3a4a986cc8715c0271e8f1c6a8ba87a28840
jle5540 <166565405+jle5540@users.noreply.github.com>
2025-02-13 13:23:18 -0500

    added VR moving, camera and setting stuff to conference room

--- Assets/Scripts/Conference Room Manager.cs
+++ Assets/Scripts/Conference Room Manager.cs
@@ -5,26 +5,12 @@ using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.UI;
 using TMPro; // Add this namespace for TMP_Dropdown
 
-public class OfficeManager : MonoBehaviour
+public class ConferenceRoomManager : MonoBehaviour
 {
-    public GameTimer timer;
-    public GameObject timerCanvas;
-    public GameObject textTimer;
     public ActionBasedSnapTurnProvider snapTurn;
     public ActionBasedContinuousTurnProvider continuousTurn;
     [SerializeField] public GameObject dropDownObject;
     private TMP_Dropdown dropDown; // Change Dropdown to TMP_Dropdown
-
-    // Start is called before the first frame update
-    void Awake()
-    {
-        if (SettingManager.Instance.EventMode == false)
-        {
-            timer.enabled = false;
-            timerCanvas.SetActive(false);
-            textTimer.SetActive(false);
-        }
-    }
 
     void Start()
     {
