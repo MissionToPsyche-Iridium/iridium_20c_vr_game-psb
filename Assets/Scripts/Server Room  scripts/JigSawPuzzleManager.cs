248e18613ca351140355783bd615b49d8367ce5c
JohnNoodle <jmp7651@psu.edu>
2025-04-10 16:41:58 -0400

    jigsaw puzzle progress

--- Assets/Scripts/Server Room  scripts/JigSawPuzzleManager.cs
+++ Assets/Scripts/Server Room  scripts/JigSawPuzzleManager.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class JigSawPuzzleManager : MonoBehaviour
-{
+{       
+
+        [Header("Game Manager")]
+        [Range(3,6)]
+        [SerializeField] private int difficulty = 4;
+
         [Header("Jigsaw Puzzle")]
         [SerializeField] private List<Texture2D> jigsawPuzzleImages;
         [SerializeField] private Transform jigsawBoard;
         [SerializeField] private Image levelSelectPrefab;
+        private List<Transform> jigsawPieces;
+        private Vector2Int dimensions;
+        private float height;
+        private float width;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +26,12 @@ public class JigSawPuzzleManager : MonoBehaviour
            image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
         }
 
+    }
+
+    void createJigsawPieces(Texture2D jigsawImage)
+    {
+        height = 1f / dimensions.y;
+        float aspectRatio = (float)jigsawImage.width / jigsawImage.height;
+        width  = aspect / dimensions.x;
     }
 }
