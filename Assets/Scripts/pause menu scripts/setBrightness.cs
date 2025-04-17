8ee224d545d00b302979f29ab4c863aeae8af282
jle5540 <166565405+jle5540@users.noreply.github.com>
2025-04-17 14:06:19 -0400

    update to brightness settings

--- Assets/Scripts/pause menu scripts/setBrightness.cs
+++ Assets/Scripts/pause menu scripts/setBrightness.cs
@@ -5,17 +5,14 @@ using UnityEngine;
 public class setBrightness : MonoBehaviour
 {
 
-    [SerializeField] private GameObject[] lightObjects;
+    [SerializeField] private GameObject lightObjects;
 
     public void SetLightIntensity(float value)
     {
-        foreach(GameObject obj in lightObjects)
-        {
-            Light lightPart = obj.GetComponent<Light>();
-            if(lightPart != null) {
-                SettingManager.Instance.Brightness = value;
-                lightPart.intensity = value;
-            }
+        Light lightPart = lightObjects.GetComponent<Light>();
+        if(lightPart != null) {
+            SettingManager.Instance.Brightness = value;
+            lightPart.intensity = value;
         }
     }
 }
