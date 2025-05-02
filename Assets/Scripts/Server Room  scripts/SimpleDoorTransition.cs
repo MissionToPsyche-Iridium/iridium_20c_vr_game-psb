a431e80fa9e9b8d37338d8627eb75126c8222571
JohnNoodle <jmp7651@psu.edu>
2025-05-02 13:22:43 -0400

    server room completed indicator

--- Assets/Scripts/Server Room  scripts/SimpleDoorTransition.cs
+++ Assets/Scripts/Server Room  scripts/SimpleDoorTransition.cs
@@ -2,28 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class SimpleDoorScript : MonoBehaviour
 {
-    public Renderer doorRenderer;
-    public Color glowColor = Color.green;
-    private Material doorMat;
     private bool isReadyToEnter = false;
+    
+    public GameObject finishText;
+    public AudioSource completedSound; 
+    
     [SerializeField] private FadeScreen fadeScreen; 
 
     void Start()
     {
-        if (doorRenderer == null)
-        {
-            //Debug.LogError("DoorRenderer not assigned! Please assign the Door's MeshRenderer.");
-            return;
-        }
-
-        doorMat = doorRenderer.material;
-
-        // Ensure glow is OFF at start
-        doorMat.DisableKeyword("_EMISSION");
-        doorMat.SetColor("_EmissionColor", Color.black);
+        finishText.SetActive(false);
         isReadyToEnter = false;
         //Debug.Log("Emission is disabled and set to black");
     }
@@ -34,19 +25,8 @@ public class SimpleDoorScript : MonoBehaviour
         if (!isReadyToEnter && GameProgressManager.Instance.AreBothMinigamesComplete())
         {
             isReadyToEnter = true;
-            EnableDoorGlow();
-        }
-    }
-
-    void EnableDoorGlow()
-    {
-        if (doorMat != null)
-        {
-            doorMat.EnableKeyword("_EMISSION");
-            glowColor = Color.green.linear * 2f;
-            doorMat.SetColor("_EmissionColor", glowColor);
-            DynamicGI.SetEmissive(doorRenderer, glowColor); // Optional
-            //Debug.Log("Glow enabled, emission color set to: " + glowColor);
+            finishText.SetActive(true);
+            completedSound.Play(); // Play the sound when both minigames are complete
         }
     }
 
